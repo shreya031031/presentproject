969500dc55a11664da8c07ee24ed3e5e590feab8
shreya031031 <katukurishreya03@gmail.com>
2023-12-17 22:02:13 +0530

    demo

---
+++ src/main/java/com/mavenweb/mavenexample/App.java
@@ -0,0 +1,13 @@
+package com.mavenweb.mavenexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
